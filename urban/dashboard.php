cba7464364deec6644362dd62cd6eb031735dbe0
majithiya aditya <147297379+majithiyaaditya@users.noreply.github.com>
2025-07-24 01:45:12 +0530

    Update dashboard.php

--- urban/dashboard.php
+++ urban/dashboard.php
@@ -5,6 +5,30 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 ?>
-<h2>Welcome, <?= $_SESSION['username'] ?>!</h2>
-<p>This is your dashboard.</p>
-<a href="logout.php">Logout</a>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script  type="text/javascript">
+        window.history.forward();
+        function noBack() {
+            window.history.forward();
+        }
+        setTimeout("noBack()", 0);
+        window.onunload = function() {
+            null;
+        }
+    </script>
+</head>
+
+<body>
+    <h2>Welcome, <?= $_SESSION['username'] ?>!</h2>
+    <p>This is your dashboard.</p>
+    <a href="logout.php">Logout</a>
+</body>
+
+</html>
